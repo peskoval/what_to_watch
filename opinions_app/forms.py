2ab09750841865cab57a128818126fe900ec641f
Aleksandra Peskova <peskoval88@gmail.com>
2025-11-22 12:23:45 +0300

    push

--- opinions_app/forms.py
+++ opinions_app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, MultipleFileField
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
 
@@ -16,5 +17,16 @@ class OpinionForm(FlaskForm):
     source = URLField(
         'Добавьте ссылку на подробный обзор фильма',
         validators=[Length(1, 256), Optional()]
+    )
+    images = MultipleFileField(
+        validators=[
+            FileAllowed(
+                ['jpg', 'jpeg', 'png', 'gif', 'bmp'],
+                message=(
+                    'Выберите файлы с расширением '
+                    '.jpg, .jpeg, .png, .gif или .bmp'
+                )
+            )
+        ]
     )
     submit = SubmitField('Добавить')
--- opinions_app/views.py
+++ opinions_app/views.py
@@ -3,6 +3,8 @@ from random import randrange
 from flask import abort, flash, redirect, render_template, url_for
 
 from . import app, db
+from .dropbox import async_upload_files_to_dropbox
+# from .dropbox import upload_files_to_dropbox
 from .forms import OpinionForm
 from .models import Opinion
 
@@ -24,17 +26,24 @@ def index_view():
 
 
 @app.route('/add', methods=['GET', 'POST'])
-def add_opinion_view():
+async def add_opinion_view():
     form = OpinionForm()
     if form.validate_on_submit():
         text = form.text.data
         if Opinion.query.filter_by(text=text).first() is not None:
             flash('Такое мнение уже было оставлено ранее!')
             return render_template('add_opinion.html', form=form)
+        # Добавьте вызов функции загрузки файлов 
+        # и передайте туда сами файлы.
+        # urls = upload_files_to_dropbox(form.images.data)
+        urls = await async_upload_files_to_dropbox(form.images.data)
         opinion = Opinion(
             title=form.title.data,
             text=text,
-            source=form.source.data
+            source=form.source.data,
+            # При создании объекта передайте все ссылки
+            # на изображения в поле images
+            images=urls
         )
         db.session.add(opinion)
         db.session.commit()
